0e63468b967ce639f1494323b8a72aac3bad6356
Jairo y Kevin <jairo.gonzalez-b@mail.escuelaing.edu.co>
2016-04-21 16:46:12 -0500

    Clases de equivalencia Jairo y kevin

---
+++ src/test/java/pruebasHistorial.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class pruebasHistorial{
+    /*CLases de Equivalecia
+    1.(Jairo) El serial del equipo solicitado es una secuencia de numeros enteros, que no contiene letra, ademas este debe ser irrepetible.
+    2.(Kevin) EL equipo solo puede tener dos estados "activo" o "desactivo", no es nulo.
+    3.(Jairo) EL historial debe tener al menos un evento o prestamo.
+    4.(Kevin) Solo puede haber una ubicacion para cada equipo.
+    5.(Jairo) La fecha limite de garantia debe ser mayor a la fecha de adquisión.
+    6.(Kevin) El equipo solo tiene un proveedor.
+    7.(Jairo) El equipo puede estar asegurado o no estar asegurado.
+    8.(Kevin) El codigo del Activo es un entero.
+    9.(Jairo) Cada equipo debe tener asociado el valor de compra como un entero.
+   10.(Kevin) Cada equipo es revisado por un laboratorista.
+   11.(Jairo) La fecha de revision del laboratorista, debe ser menor o igual a la fecha actual.
+   12.(Kevin) La fecha Vo Bo debe ser menor o igual a la fecha actual.
+    */
+    
+    
+}
