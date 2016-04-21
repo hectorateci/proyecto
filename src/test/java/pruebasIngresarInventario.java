2136cc0cb12571d7c1c886f30695970b7a9b596f
nicomoregu <nicomoregu@gmail.com>
2016-04-21 16:32:13 -0500

    enunciado pruebas inventario

--- src/test/java/pruebasIngresarInventario.java
+++ src/test/java/pruebasIngresarInventario.java
@@ -18,4 +18,82 @@ import static org.junit.Assert.*;
 public class pruebasIngresarInventario {
     
     
+    /**
+    *Se ingresa al inventario un equipo que el modelo ya esta existente con un numero de serial y un numero de placa.
+    *Tipo de prueba: positiva.
+    *
+    */
+    @Test
+    public void ingresarInventarioModeloExistenteSerialPlaca(){
+        
+    }
+    
+    
+    /**
+    *Se ingresa al inventario un equipo que el modelo ya esta existente con solo un numero de serial.
+    *Tipo de prueba: positiva.
+    *
+    */
+    @Test
+    public void ingresarInventarioModeloExistenteSerial(){
+        
+    }
+    
+    
+    /**
+    *Se ingresa al inventario un equipo que el modelo no esta existente con un numero de serial y un numero de placa.
+    *Tipo de prueba: positiva.
+    *
+    */
+    @Test
+    public void ingresarInventarioModeloNoExistenteSerialPlaca(){
+        
+    }
+    
+    
+    /**
+    *Se ingresa al inventario un equipo que el modelo no esta existente con solo un numero de serial.
+    *Tipo de prueba: positiva.
+    *
+    */
+    @Test
+    public void ingresarInventarioModeloNoExistenteSerial(){
+        
+    }
+    
+    /**
+     * Se ingresa al inventario un equipo que el modelo esta existente con un numero de serial existente.
+     * Tipo de prueba: negativa
+     */
+    @Test
+    public void ingresarInventarioModeloExistenteSerialExistente(){
+    
+    }
+    
+    /**
+     * Se ingresa al inventario un equipo que el modelo esta existente con un numero de placa existente.
+     * Tipo de prueba: negativa
+     */
+    @Test
+    public void ingresarInventarioModeloExistentePlacaExistente(){
+    
+    }
+    
+    /**
+     * Se ingresa al inventario un equipo que el modelo no esta existente con un numero de serial existente.
+     * Tipo de prueba: negativa
+     */
+    @Test
+    public void ingresarInventarioModeloNoExistenteSerialExistente(){
+    
+    }
+    
+    /**
+     * Se ingresa al inventario un equipo que el modelo no esta existente con un numero de placa existente.
+     * Tipo de prueba: negativa
+     */
+    @Test
+    public void ingresarInventarioModeloNoExistentePlacaExistente(){
+    
+    }
 }
