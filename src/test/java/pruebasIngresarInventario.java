6a50a4a1502cc19079e9da942b58ff4c53bcf801
nicomoregu <nicomoregu@gmail.com>
2016-04-21 16:04:30 -0500

    Carpeta de pruebas

---
+++ src/test/java/pruebasIngresarInventario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 2098874
+ */
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class pruebasIngresarInventario {
+    
+    
+}
