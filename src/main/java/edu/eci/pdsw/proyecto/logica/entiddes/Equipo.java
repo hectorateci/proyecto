557223cb459d4dba849df4d3634017114e68b990
sebastianamaya <sebastian.amaya@mail.escuelaing.edu.co>
2016-04-24 18:23:59 -0500

    logica entidades

---
+++ src/main/java/edu/eci/pdsw/proyecto/logica/entiddes/Equipo.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.pdsw.proyecto.logica.entiddes;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author 2098654
+ */
+public class Equipo {
+
+
+
+    private String nombre;
+    private String marca;
+    private Boolean estado;
+    private String descripcion;
+    private Date fecha_adquisicion ;
+    private Date fecha_garantia ;
+    private int valor  ;
+    private String orden_compra;
+    private String codigo_activo;
+    private Boolean asegurado ;
+    private String categoria;
+    private int tiempo_uso;
+    private String proveedor;
+    private String descripcion_estado;
+    
+    public  Equipo (String nombre,String marca,Boolean estado,String descripcion,Date fecha_adquisicion , Date fecha_garantia ,int valor  ,String orden_compra,String codigo_activo,Boolean asegurado ,String categoria,int tiempo_uso,String proveedor,String descripcion_estado){
+        this .nombre=nombre;
+        this.marca=marca;
+        this.estado=estado;
+        this.descripcion=descripcion;
+        this.fecha_adquisicion=fecha_adquisicion;
+        this.fecha_garantia=fecha_garantia;
+        this.valor=valor;
+        this.orden_compra=orden_compra;
+        this.codigo_activo=codigo_activo;
+        this.asegurado=asegurado;
+        this.categoria=categoria;
+        this.tiempo_uso=tiempo_uso;
+        this.proveedor=proveedor;
+        this.descripcion_estado=descripcion_estado;
+                
+    
+            
+}
+    
+        public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha_adquisicion() {
+        return fecha_adquisicion;
+    }
+
+    public void setFecha_adquisicion(Date fecha_adquisicion) {
+        this.fecha_adquisicion = fecha_adquisicion;
+    }
+
+    public Date getFecha_garantia() {
+        return fecha_garantia;
+    }
+
+    public void setFecha_garantia(Date fecha_garantia) {
+        this.fecha_garantia = fecha_garantia;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getOrden_compra() {
+        return orden_compra;
+    }
+
+    public void setOrden_compra(String orden_compra) {
+        this.orden_compra = orden_compra;
+    }
+
+    public String getCodigo_activo() {
+        return codigo_activo;
+    }
+
+    public void setCodigo_activo(String codigo_activo) {
+        this.codigo_activo = codigo_activo;
+    }
+
+    public Boolean getAsegurado() {
+        return asegurado;
+    }
+
+    public void setAsegurado(Boolean asegurado) {
+        this.asegurado = asegurado;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getTiempo_uso() {
+        return tiempo_uso;
+    }
+
+    public void setTiempo_uso(int tiempo_uso) {
+        this.tiempo_uso = tiempo_uso;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getDescripcion_estado() {
+        return descripcion_estado;
+    }
+
+    public void setDescripcion_estado(String descripcion_estado) {
+        this.descripcion_estado = descripcion_estado;
+    }
+
+}
+ 
---
+++ src/main/java/edu/eci/pdsw/proyecto/logica/entiddes/prestamo.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.pdsw.proyecto.logica.entiddes;
+
+import java.sql.Date;
+import java.util.*;
+
+/**
+ *
+ * @author 2098654
+ */
+public class prestamo {
+    private Date fechaSolicitud ;
+    private Date fechaDevolucion ;
+    private Usuario Usuario ;
+    Set<Equipo> items;
+    private String detalle;
+    
+    public prestamo(     Date fechaSolicitud ,Date fechaDevolucion,Usuario Usuario ,Set<Equipo> items,
+     String detalle ) {
+        this.Usuario=Usuario;
+        this.detalle=detalle;
+        this.fechaDevolucion=fechaDevolucion;
+        this.fechaSolicitud=fechaSolicitud;
+  
+    
+    }
+    public Date getFechaSolicitud() {
+        return fechaSolicitud;
+    }
+
+    public void setFechaSolicitud(Date fechaSolicitud) {
+        this.fechaSolicitud = fechaSolicitud;
+    }
+
+    public Date getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(Date fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public Usuario getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(Usuario Usuario) {
+        this.Usuario = Usuario;
+    }
+
+    public Set<Equipo> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<Equipo> items) {
+        this.items = items;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    
+}
