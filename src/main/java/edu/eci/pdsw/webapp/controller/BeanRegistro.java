967144d1e04327956810d9d9286d79f6c5758a73
Juan Camilo Herrera <juan.c.herrerav@hotmail.com>
2016-04-27 10:36:28 -0500

    beanRegistro

---
+++ src/main/java/edu/eci/pdsw/webapp/controller/BeanRegistro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.pdsw.webapp.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author JuanHerrera 
+ */
+@SessionScoped
+@ManagedBean(name = "beanRegistro")
+public class BeanRegistro {
+    
+}
